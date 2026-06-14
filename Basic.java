b7e32c17917efbcf2e78a02b416c54fe7ca4e911
Madhurij123 <madhurijadhav1698@gmail.com>
2026-06-14 23:23:02 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,12 @@
+public class Basic 
+{
+   void show()
+   {
+    System.out.println("Welcome...!!!");
+   }
+   public static void main(String args[])
+   {
+    Basic h = new Basic();
+    h.show();
+   }
+}
---
+++ evenodd.java
@@ -0,0 +1,16 @@
+public class evenodd
+{
+    public static void main(String args[])
+    {
+        int num = 3;
+
+        if (num % 2 == 0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
