8ab1e0b2bb937c09a4a927f0ff15fcee0638b049
Madhurij123 <madhurijadhav1698@gmail.com>
2026-08-04 23:56:19 +0530

    Add files via upload

--- Book.java
+++ Book.java
@@ -1,85 +1,68 @@
-import java.util.Objects;
+import java.util.Scanner;
 
-public class Book implements Comparable<Book>{
-
+public class Book implements Comparable<Book> {
 	private int id;
-	private String name;
-	private String subject;
+	private int quantity;
+	private String authorName;
 	private double price;
 	
 	public Book()
 	{
 		
 	}
-
-	public Book(int id, String name, String subject, double price) {
+	public Book(int id, int quantity, String authorName, double price) {
 		super();
 		this.id = id;
-		this.name = name;
-		this.subject = subject;
+		this.quantity = quantity;
+		this.authorName = authorName;
 		this.price = price;
 	}
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getName() {
-		return name;
+	public int getQuantity() {
+		return quantity;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
 	}
-
-	public String getSubject() {
-		return subject;
+	public String getAuthorName() {
+		return authorName;
 	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
 	}
-
 	public double getPrice() {
 		return price;
 	}
-
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	@Override
-    public int hashCode()
-    {
-		return Objects.hash(id);
-    }
-
-	@Override
-	public boolean equals(Object obj)
+	public  void accept()
 	{
-		if(this == obj)
-			return true;
-		if(obj == null)
-			return false;
-		if(!(obj instanceof Book))
-			return true;
-		Book other = (Book) obj;
-		return this.id == other.id;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a book id");
+		id = sc.nextInt();
+		System.out.println("Enter a quantity:");
+		quantity = sc.nextInt();
+		System.out.println("Enter a Author name:");
+		authorName = sc.next();
+		System.out.println("Enter a Price:");
+		price = sc.nextDouble();
 	}
-	
 	@Override
 	public String toString() {
-		return "Book [id=" + id + ", name=" + name + ", subject=" + subject + ", price=" + price + "]";
+		return "Book [id=" + id + ", quantity=" + quantity + ", authorName=" + authorName + ", price=" + price + "]";
 	}
-
 	@Override
 	public int compareTo(Book other) {
-		int diff = this.id - other.id;
+		int diff = (int) (other.price - this.price);
 		return diff;
 	}
 	
+	
+
 }
--- Program.java
+++ Program.java
@@ -1,13 +1,106 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Program {
+	
+	public static int menuList()
+	{
+		int ch;
+		System.out.println("0.Exit");
+		System.out.println("1.Add New Book In list");
+		System.out.println("2.Display all BOOK in Forward Order");
+		System.out.println("3.Display all BOOK in Revers Order");
+		System.out.println("4.Delete a Book at given index");
+		System.out.println("5.Sort all book by Price in desending order");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Choice:");
+		ch = sc.nextInt();
+		return ch;
+
+	}
 
 	public static void main(String[] args) {
 
-		Box b1 = new Box(1, 2, 3);
-		Box b2 = new Box(3, 2, 3);
+		//Book[]  b = new Book[5];
 		
-		System.out.println(b1.hashCode());
-		System.out.println(b2.hashCode());
+	    List<Book> list = new ArrayList<Book>();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int ch;
+		int count = 0;
+		
+		while((ch = menuList())!= 0)
+		{
+			switch(ch)
+			{
+			case 1:
+				System.out.println("--------Book Management System---------------------");
+				System.out.print("Enter a No.of Books want to add:");
+				int n = sc.nextInt();
+				System.out.println("---------------------------------------------------");
+				for(int i = 0; i < n; i++)
+				{
+					Book b = new Book();
+					b.accept();
+					list.add(b);
+					System.out.println("New Book Added Successfully..!!");
+					System.out.println("---------------------------------------------------");
+
+				}
+				break;
+				
+			case 2:
+				Iterator<Book> trav = list.iterator();
+				while(trav.hasNext())
+				{
+					Book e = trav.next();
+					System.out.println(e);
+					System.out.println("---------------------------------------------------");
+				}
+				System.out.println();
+				break;
+				
+			case 3:
+				ListIterator<Book> trav1 = list.listIterator(list.size());
+
+			    while (trav1.hasPrevious()) {
+			        Book b = trav1.previous();
+			        System.out.println(b);
+					System.out.println("---------------------------------------------------");
+			    }
+			    System.out.println();
+			    break;
+			    
+			case 4:
+				System.out.println("Enter Id WANT TO Delete:");
+				int key = sc.nextInt();
+				list.remove(key);
+				System.out.println("Book Deleted Succsfully..!!");
+				break;
+				
+			case 5:
+				Collections.sort(list);
+				for(Book bk : list)
+				{
+					System.out.println(bk);
+					System.out.println("---------------------------------------------------");
+				}
+				break;
+				
+			default:
+				System.out.println("Invalid Choice...!!");
+				
+			}
+		}
+		
+		
 	}
 
 }
