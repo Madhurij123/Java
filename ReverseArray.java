b5250dcb65bf03dc8fcaa8e3e989d3bf6f8add90
Madhurij123 <madhurijadhav1698@gmail.com>
2026-08-06 23:49:55 +0530

    Add files via upload

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,26 +1,34 @@
-package clg.dkte;
-
-import java.util.Arrays;
+package dsa1.array;
 
 public class ReverseArray {
 
-	public static void main(String[] args) {
-		int arr[] = {1,2 ,3,4,5};
+	static void reverseArray(int[] arr)
+	{
+		int n = arr.length;
 		
-		int l = 0;
-		int h = arr.length -1;
-		
-		while(l <= h)
-		{
-			int temp = arr[l];
-			arr[l] = arr[h];
-			arr[h] = temp;
-			
-			
-			l++;
-			h--;
-		}
-		System.out.println(Arrays.toString(arr));
-	}
+		int[] temp = new int[n];
 
+		//Revrse the element
+		for(int i = 0; i < n; i++) {
+			temp[i] = arr[n - i - 1];   //--------5 - 0 - 1 = 4  when size is 5 place last elemnt in 4th position
+		}
+		
+		//COPY BACK TO original 
+		for(int i = 0; i < n; i ++) {
+			arr[i] = temp[i];
+		}
+		
+		
+	}
+	public static void main(String[] args)
+	{
+		int[] arr = {1, 2, 3, 4, 5};
+		
+		reverseArray(arr);
+		
+		for(int a : arr)
+		{
+			System.out.println(a);
+		}
+	}
 }
